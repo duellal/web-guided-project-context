837a7d599af4f4088fa8c400b84a652dcefbcd7e
duellal <alexandriaduell@gmail.com>
2022-09-18 14:24:19 -0400

    Using Consumer in Siblings file

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -2,17 +2,25 @@ import React, { useContext } from "react";
 import { FamilyContext } from "../contexts/FamilyContext";
 
 const Siblings = () => {
-  const family = useContext(FamilyContext)
+  // const family = useContext(FamilyContext)
 
   return (
-    <section className="parents">
-      {family.siblings.map((p) => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
-    </section>
+    <FamilyContext.Consumer>
+      {
+        (family) => {
+          return (
+            <section className="parents">
+              {family.siblings.map((p) => (
+                <div className="person" key={p.name}>
+                  <img width="150" src={p.img} alt={p.name} />
+                  <strong>{p.name}</strong>
+                </div>
+              ))}
+            </section>
+          )
+        }
+      }
+    </FamilyContext.Consumer>
   );
 };
 
